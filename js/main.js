fff08f8ec7de5a3caf6d54c993b32462a829dceb
Miguelangel14-cell <145133815+Miguelangel14-cell@users.noreply.github.com>
2023-12-09 00:12:49 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
-navbar-light bg-dark">
+document.getElementById("header").innerHTML=` 
+<link rel="stylesheet" href="css/styles1.css">
+<nav class="navbar navbar-expand-sm
+navbar-light bg-dark" style="background-color: #e3f2fd;">
 <img src='./imagenes/LOGO.png'>
 <div class="container">
 <a class="navbar-brand" style="color: #f8f9fa;" href="#">Arcade Argentina</a>
@@ -15,24 +17,28 @@ aria-expanded="false" aria-label="Toggle navigation">
 class="visually-hidden">(current)</span></a>
 </li>
 <li class="nav-item">
-<a class="nav-link" style="color: #f8f9fa;" href="#">Link</a>
+<a class="nav-link" style="color: #f8f9fa;" href="https://github.com/SContrera/arcadeArgB">Link</a>
 </li>
 <li class="nav-item dropdown">
 <a class="nav-link dropdown-toggle" style="color: #f8f9fa;" href="#" id="dropdownId"
 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">CRUD</a>
-
 <div class="dropdown-menu" aria-labelledby="dropdownId">
 <a class="dropdown-item" href="./productos.html">Productos</a>
-<a class="dropdown-item" href="#">Action 2</a>
+<a class="dropdown-item" href="./arcade1/index.html">Arcade Argentina Web</a>
 </div>
 </li>
 </ul>
 <center><img src="./imagenes/ARCADE1.gif" style="width:40%"></center>
-<form class="d-flex my-2 my-lg-0">
-<input class="form-control me-sm-2" type="text" placeholder="Search">
+<a href="#"><i class="fa-brands fa-facebook"></i></a>&nbsp;&nbsp;
+<a href="#"><i class="fa-brands fa-instagram"></i></a>&nbsp;&nbsp;
+<a href="#"><i class="fa-brands fa-twitter"></i></a>&nbsp;&nbsp;
+<a href="https://web.whatsapp.com/"><i _ngcontent-tau-c135="" class="fab fa-whatsapp"></i></a>&nbsp;&nbsp;
+<form action="./consulta.html" method="GET" class="d-flex my-2 my-lg-0">
+<input class="form-control me-sm-2" type="text" id="productos" placeholder="Search">
 <button class="btn btn-outline-success my-2 my-sm-0"
 type="submit">Search</button>
 </form>
 </div>
 </div>
-</nav>`
+</nav>
+`
